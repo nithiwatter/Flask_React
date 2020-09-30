147cd7560d3c3b55d47b64ddab35a59187e5b85a
nithiwatter <nithiwatter@gmail.com>
2020-09-30 09:54:58 +0700

    transition is dumb af whotf made css

--- frontend/src/common/table/RankTable.js
+++ frontend/src/common/table/RankTable.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 // some dates from the API have trouble with being parsed
 // import { format } from 'date-fns';
 import {
@@ -12,18 +12,18 @@ import {
   Paper,
   Typography,
   Button,
-  LinearProgress,
   Grow,
+  LinearProgress,
   makeStyles,
-} from '@material-ui/core';
-import StarRateIcon from '@material-ui/icons/StarRate';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import SmallMenu from './SmallMenu';
+} from "@material-ui/core";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+
+import RankTableData from "./RankTableData";
 
 const useStyles = makeStyles((theme) => ({
   mainTitleContainer: {
-    display: 'flex',
+    display: "flex",
     marginBottom: theme.spacing(2),
   },
   mainTitle: {
@@ -39,48 +39,12 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.common.white,
   },
-  rankContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  rank: {
-    fontWeight: 700,
-  },
-  titleContainer: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flexShrink: 0,
-  },
-  image: {
-    maxWidth: 75,
-    height: 'auto',
-  },
-  textContainer: {
-    marginLeft: theme.spacing(2),
-  },
-  title: {
-    color: theme.palette.primary.main,
-    fontWeight: 700,
-    textDecoration: 'none',
-    '&:active': {
-      color: theme.palette.primary.main,
-    },
-    '&:visited': {
-      color: theme.palette.primary.main,
-    },
-    '&:hover': {
-      textDecoration: 'underline',
-    },
-  },
   paginationContainer: {
-    marginLeft: 'auto',
+    marginLeft: "auto",
   },
   bottomPaginationContainer: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
     marginTop: theme.spacing(2),
   },
 }));
@@ -91,58 +55,61 @@ const RankTable = (props) => {
 
   return (
     <React.Fragment>
-      {loading ? <LinearProgress></LinearProgress> : null}
+      <div>
+        <div className={classes.mainTitleContainer}>
+          <Typography variant="h5" className={classes.mainTitle}>
+            Top Anime Series
+          </Typography>
+          <Button
+            variant="outlined"
+            color="primary"
+            className={classes.button}
+            disabled={loading}
+          >
+            My Favorites
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            className={classes.button}
+            disabled={loading}
+          >
+            Watch Later
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            className={classes.button}
+            disabled={loading}
+          >
+            Waifu Tier List
+          </Button>
+          <div className={classes.paginationContainer}>
+            {page > 0 ? (
+              <Button
+                startIcon={<ChevronLeftIcon></ChevronLeftIcon>}
+                component={NavLink}
+                to={`/topAnime?page=${page - 1}`}
+                disabled={loading}
+              >
+                Prev 50
+              </Button>
+            ) : null}
 
-      {!loading ? (
-        <React.Fragment>
-          <Grow in={true} style={{ transformOrigin: '0 0 0' }}>
-            <div>
-              <div className={classes.mainTitleContainer}>
-                <Typography variant="h5" className={classes.mainTitle}>
-                  Top Anime Series
-                </Typography>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  className={classes.button}
-                >
-                  My Favorites
-                </Button>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  className={classes.button}
-                >
-                  Watch Later
-                </Button>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  className={classes.button}
-                >
-                  Waifu Tier List
-                </Button>
-                <div className={classes.paginationContainer}>
-                  {page > 0 ? (
-                    <Button
-                      startIcon={<ChevronLeftIcon></ChevronLeftIcon>}
-                      component={NavLink}
-                      to={`/topAnime?page=${page - 1}`}
-                    >
-                      Prev 50
-                    </Button>
-                  ) : null}
-
-                  <Button
-                    endIcon={<ChevronRightIcon></ChevronRightIcon>}
-                    component={NavLink}
-                    to={`/topAnime?page=${page + 1}`}
-                  >
-                    Next 50
-                  </Button>
-                </div>
-              </div>
-
+            <Button
+              endIcon={<ChevronRightIcon></ChevronRightIcon>}
+              component={NavLink}
+              to={`/topAnime?page=${page + 1}`}
+              disabled={loading}
+            >
+              Next 50
+            </Button>
+          </div>
+        </div>
+        {loading ? <LinearProgress color="secondary"></LinearProgress> : null}
+        {!loading ? (
+          <React.Fragment>
+            <Grow in={true} style={{ transformOrigin: "top center" }}>
               <TableContainer component={Paper}>
                 <Table className={classes.table}>
                   <TableHead>
@@ -177,82 +144,38 @@ const RankTable = (props) => {
                     </TableRow>
                   </TableHead>
                   <TableBody>
-                    {data.map((entry, i) => (
-                      <TableRow key={entry.anime_id}>
-                        <TableCell>
-                          <Typography variant="h4" className={classes.rank}>
-                            {page * size + i + 1}
-                          </Typography>
-                        </TableCell>
-                        <TableCell align="left">
-                          <div className={classes.titleContainer}>
-                            <div className={classes.imageContainer}>
-                              <img
-                                src={entry.mal_anime_image_path}
-                                className={classes.image}
-                                alt=""
-                              ></img>
-                            </div>
-
-                            <div className={classes.textContainer}>
-                              <Typography
-                                variant="h6"
-                                className={classes.title}
-                                component={NavLink}
-                                to={`/anime/${entry.anime_id}`}
-                              >
-                                {entry.name}
-                              </Typography>
-                              <Typography variant="body2">
-                                {entry.anime_type}
-                              </Typography>
-                              <Typography variant="body2">
-                                {entry.airing_str}
-                              </Typography>
-                              <Typography variant="body2">
-                                {entry.members.toLocaleString()} members
-                              </Typography>
-                            </div>
-                          </div>
-                        </TableCell>
-                        <TableCell align="right">
-                          <div className={classes.rankContainer}>
-                            <StarRateIcon></StarRateIcon>
-                            <div>{entry.rating}</div>
-                          </div>
-                        </TableCell>
-                        <TableCell align="right">N/A</TableCell>
-                        <TableCell align="right">
-                          <SmallMenu></SmallMenu>
-                        </TableCell>
-                      </TableRow>
-                    ))}
+                    <RankTableData
+                      data={data}
+                      page={page}
+                      size={size}
+                    ></RankTableData>
                   </TableBody>
                 </Table>
               </TableContainer>
-              <div className={classes.bottomPaginationContainer}>
-                {page > 0 ? (
-                  <Button
-                    startIcon={<ChevronLeftIcon></ChevronLeftIcon>}
-                    component={NavLink}
-                    to={`/topAnime?page=${page - 1}`}
-                  >
-                    Prev 50
-                  </Button>
-                ) : null}
+            </Grow>
 
+            <div className={classes.bottomPaginationContainer}>
+              {page > 0 ? (
                 <Button
-                  endIcon={<ChevronRightIcon></ChevronRightIcon>}
+                  startIcon={<ChevronLeftIcon></ChevronLeftIcon>}
                   component={NavLink}
-                  to={`/topAnime?page=${page + 1}`}
+                  to={`/topAnime?page=${page - 1}`}
                 >
-                  Next 50
+                  Prev 50
                 </Button>
-              </div>
+              ) : null}
+
+              <Button
+                endIcon={<ChevronRightIcon></ChevronRightIcon>}
+                component={NavLink}
+                to={`/topAnime?page=${page + 1}`}
+              >
+                Next 50
+              </Button>
             </div>
-          </Grow>
-        </React.Fragment>
-      ) : null}
+          </React.Fragment>
+        ) : null}
+      </div>
     </React.Fragment>
   );
 };
---
+++ frontend/src/common/table/RankTableData.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Typography, TableRow, TableCell, makeStyles } from "@material-ui/core";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import SmallMenu from "./SmallMenu";
+
+const useStyles = makeStyles((theme) => ({
+  rankContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  rank: {
+    fontWeight: 700,
+  },
+  titleContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flexShrink: 0,
+  },
+  image: {
+    maxWidth: 75,
+    height: "auto",
+  },
+  textContainer: {
+    marginLeft: theme.spacing(2),
+  },
+  title: {
+    color: theme.palette.primary.main,
+    fontWeight: 700,
+    textDecoration: "none",
+    "&:active": {
+      color: theme.palette.primary.main,
+    },
+    "&:visited": {
+      color: theme.palette.primary.main,
+    },
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+}));
+
+const RankTableData = (props) => {
+  const classes = useStyles();
+  const { data, page, size } = props;
+
+  return (
+    <React.Fragment>
+      {data.map((entry, i) => (
+        <TableRow key={entry.anime_id}>
+          <TableCell>
+            <Typography variant="h4" className={classes.rank}>
+              {page * size + i + 1}
+            </Typography>
+          </TableCell>
+          <TableCell align="left">
+            <div className={classes.titleContainer}>
+              <div className={classes.imageContainer}>
+                <img
+                  src={entry.mal_anime_image_path}
+                  className={classes.image}
+                  alt=""
+                ></img>
+              </div>
+
+              <div className={classes.textContainer}>
+                <Typography
+                  variant="h6"
+                  className={classes.title}
+                  component={NavLink}
+                  to={`/anime/${entry.anime_id}`}
+                >
+                  {entry.name}
+                </Typography>
+                <Typography variant="body2">{entry.anime_type}</Typography>
+                <Typography variant="body2">{entry.airing_str}</Typography>
+                <Typography variant="body2">
+                  {entry.members.toLocaleString()} members
+                </Typography>
+              </div>
+            </div>
+          </TableCell>
+          <TableCell align="right">
+            <div className={classes.rankContainer}>
+              <StarRateIcon></StarRateIcon>
+              <div>{entry.rating}</div>
+            </div>
+          </TableCell>
+          <TableCell align="right">N/A</TableCell>
+          <TableCell align="right">
+            <SmallMenu></SmallMenu>
+          </TableCell>
+        </TableRow>
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default RankTableData;
