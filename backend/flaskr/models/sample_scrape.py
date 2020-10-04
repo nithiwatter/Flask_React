1a427abeb878d8b9035ef30b038bbab9834a358a
nithiwatter <nithiwatter@gmail.com>
2020-10-05 01:15:41 +0700

    m

--- backend/flaskr/models/sample_scrape.py
+++ backend/flaskr/models/sample_scrape.py
@@ -36,9 +36,8 @@ num_images = 0
 
 # cur_path = os.path.dirname(__file__)
 # new_path = os.path.relpath('animes_json.txt', cur_path)
-print(os.getcwd())
 new_path = os.path.join(os.path.dirname(os.getcwd()), 'animes_json.txt')
-print(new_path)
+
 with open(new_path, 'r') as json_file:
     data = json.load(json_file)
     for i in range(len(data)):
