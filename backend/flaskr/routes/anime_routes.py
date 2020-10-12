1d81ee9475efccb4514646493974b1cbfd56a795
suryakorrapati <71289494+suryakorrapati@users.noreply.github.com>
2020-10-12 16:14:48 -0400

    Update anime_routes.py

--- backend/flaskr/routes/anime_routes.py
+++ backend/flaskr/routes/anime_routes.py
@@ -3,8 +3,8 @@ import flaskr.controllers.anime_controllers as main_controller
 
 bp = Blueprint('anime', __name__, url_prefix='/api/anime')
 
-# cannot use a normal decorator syntax as it needs def:
-# just call the function directlygit push
+# Can't use normal decorator syntax -> needs def:
+# Just call the function directly git push
 bp.route('/all', methods=['GET'])(main_controller.test_method)
 bp.route('/searchLists', methods=['GET'])(main_controller.get_search_lists)
 bp.route('/topAnime', methods=['GET'])(main_controller.get_top_50_anime)
