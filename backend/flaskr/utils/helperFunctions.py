3d2dfb9bc16dcf90945558d7f43a25e6df5c07c9
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2020-11-08 12:54:51 +0700

    fix helper

--- backend/flaskr/utils/helperFunctions.py
+++ backend/flaskr/utils/helperFunctions.py
@@ -1,7 +1,7 @@
-# Funcation takes in page number and size of page and returns offset information
+# Function takes in page number and size of page and returns offset information
 def getPagination(page, size):
-    limit = size 
-    #Generally setting size=50
-    offset = page * total
+    limit = size
+    # Generally setting size=50
+    offset = page * limit
 
     return limit, offset
